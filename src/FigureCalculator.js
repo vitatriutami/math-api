438699bcd2b6367fff774e0c9acf555960cb0ceb
Vita Tri Utami <110320983+vitatriutami@users.noreply.github.com>
2024-10-14 18:49:58 +0700

    fix: add http server testing multiply divide rectangle triangle

--- src/FigureCalculator.js
+++ src/FigureCalculator.js
@@ -3,23 +3,47 @@ class FigureCalculator {
     this._mathBasic = mathBasic;
   }
 
+  _verifyArgs(args, expectedArgsCount) {
+    if (args.length !== expectedArgsCount) {
+      throw new Error(`fungsi hanya menerima ${expectedArgsCount} parameter`);
+    }
+
+    args.forEach((arg) => {
+      if (typeof arg !== 'number') {
+        throw new Error('fungsi hanya menerima parameter number');
+      }
+    });
+
+    return args;
+  }
+
   calculateRectanglePerimeter(...args) {
-    if (args.length !== 2) {
-      throw new Error('fungsi hanya menerima dua parameter');
-    }
-
-    const [length, width] = args;
-
-    if (typeof length !== 'number' || typeof width !== 'number') {
-      throw new Error('fungsi hanya menerima parameter number');
-    }
+    const [length, width] = this._verifyArgs(args, 2);
 
     // formula: (2 * (length + width))
     return this._mathBasic.multiply(2, this._mathBasic.add(length, width));
   }
-  calculateRectangleArea() { }
-  calculateTrianglePerimeter() { }
-  calculateTriangleArea() { }
+
+  calculateRectangleArea(...args) {
+    const [length, width] = this._verifyArgs(args, 2);
+
+    // formula: (length * width)
+    return this._mathBasic.multiply(length, width);
+  }
+
+  calculateTrianglePerimeter(...args) {
+    const [sideA, sideB, base] = this._verifyArgs(args, 3);
+
+    // formula: (sideA + sideB + base)
+    return this._mathBasic.add(sideA, this._mathBasic.add(sideB, base));
+  }
+
+  calculateTriangleArea(...args) {
+    const [base, height] = this._verifyArgs(args, 2);
+
+    // formula: ((base * height) / 2)
+    return this._mathBasic.divide(this._mathBasic.multiply(base, height), 2);
+  }
 }
 
 module.exports = FigureCalculator;
